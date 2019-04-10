99d7aed12414827923b74faaa776d0ecaa4f4749
freddyh <freddhj@gmail.com>
2019-04-10 13:53:17 -0700

    fixed swiftlint warnings

--- Source/WSTag.swift
+++ Source/WSTag.swift
@@ -9,36 +9,36 @@
 import Foundation
 
 public struct WSTag: Hashable {
-    
+
     public let text: String
     public let phoneNumber: String?
     public let email: String?
-    public let info: Any?
-    
+    public let metadata: Any?
+
     public init(_ text: String) {
         self.text = text
         self.phoneNumber = nil
         self.email = nil
-        self.info = nil
+        self.metadata = nil
     }
-    
-    public init(text: String, phoneNumber: String?, email: String?, info: Any?) {
+
+    public init(text: String, phoneNumber: String?, email: String?, metadata: Any?) {
         self.text = text
         self.phoneNumber = phoneNumber
         self.email = email
-        self.info = info
+        self.metadata = metadata
     }
-    
+
     public func hash(into hasher: inout Hasher) {
         hasher.combine(self.text)
         hasher.combine(self.phoneNumber)
         hasher.combine(self.email)
     }
-    
+
     public func equals(_ other: WSTag) -> Bool {
         return self.text == other.text && self.phoneNumber == other.phoneNumber && self.email == other.email
     }
-    
+
 }
 
 public func == (lhs: WSTag, rhs: WSTag) -> Bool {
